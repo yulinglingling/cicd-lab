c6fd41c7f3b6adeb85cd5882570f2f7228ed5591
yulinglingling <a0909992885@gmail.com>
2026-05-22 07:41:27 +0000

    ci: add hello.yaml

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,8 @@ export function buildApp(options: FastifyServerOptions = {}) {
 
   app.get('/health', async () => {
     return {
-      // status: 'ok'
-      status: 'not ok'
+      status: 'ok'
+      // status: 'not ok'
     };
   });
 
